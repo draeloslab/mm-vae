Fixed reconstruction output normalization and added MoE mmvae file to train  mutlimodal vae

loss_bce.py
@@ -2,7 +2,7 @@
 import torch
 from torch.nn import functional as F
 
-def loss_function_bce(recon_x, x, mu, logvar):
+def loss_function_bce(recon_x, x, mu, logvar, beta):
     if x.shape[-1] == 28:
         BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
     else:
@@ -14,4 +14,4 @@ def loss_function_bce(recon_x, x, mu, logvar):
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return BCE + KLD, BCE, KLD
+    return BCE + beta * KLD, BCE, KLD

loss_mse.py
@@ -2,7 +2,7 @@
 import torch
 from torch.nn import functional as F
 
-def loss_function(recon_x, x, mu, logvar, beta):
+def loss_function_mse(recon_x, x, mu, logvar, beta):
     if x.shape[-1] == 28:
         MSE = F.mse_loss(recon_x, x.view(-1, 784), reduction='sum')
     else:

main.py
@@ -13,7 +13,6 @@ from vae_svhn import*
 from vae_mnist import*
 from train import*
 from latent_regularizer import*
-from loss_mse import*
 
 def load_config(config_name):
     with open(os.path.join(config_name)) as file:
@@ -24,6 +23,10 @@ config = load_config("config.yaml")
 
 random_seed = config['data']['random_seed']
 dataset = config['data']['dataset']
+if dataset == 'MNIST':
+    loss_type = 'bce'
+elif dataset == 'SVHN':
+    loss_type = 'mse'
 epochs = config['training_params']['epochs']
 batch_size = config['training_params']['batch_size']
 log_interval = config['training_params']['log_interval']
@@ -44,7 +47,7 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if accelerator else {}
 # initialize MNIST normalization and standardization (z-score) transforms with pre-calculated mean and std values
 mnist_norm = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(0.1307, 0.3801)
+        #transforms.Normalize(0.1307, 0.3801)
     ])
 
 # load in MNIST train and test data
@@ -59,7 +62,7 @@ test_loader_MNIST = torch.utils.data.DataLoader(
 # initialize SVHN normalization and standardization (z-score) transforms pre-calculated mean and std values 
 svhn_norm = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4376821, 0.4437697, 0.47280442), (0.19803012, 0.20101562, 0.19703614))
+        #transforms.Normalize((0.4376821, 0.4437697, 0.47280442), (0.19803012, 0.20101562, 0.19703614))
     ])
 
 # load in SVHN train and test data 
@@ -100,42 +103,42 @@ output_dir = os.makedirs(output_folder, exist_ok=True)
 
 if __name__ == "__main__":
     total_train_loss = []
-    total_bce_loss = []
+    total_recon_loss = []
     total_kld_loss = []
     total_test_loss = []
-    total_bce_test_loss = []
+    total_recon_test_loss = []
     total_kld_test_loss = []
     for epoch in range(1, epochs + 1):
-        # train(epoch, model, train_loader_MNIST, device, optimizer, log_interval)
-        # test(epoch, model, test_loader_MNIST, device, batch_size, output_folder)
+        # train(epoch, model, train_loader, device, optimizer, log_interval)
+        # test(epoch, model, test_loader, device, batch_size, output_folder)
         # with torch.no_grad():
         #     sample = torch.randn(64, 20).to(device)
         #     sample = model.decode(sample).cpu()  
         #     save_image(sample.view(64, 1, 28, 28),
         #                output_folder + '/sample_' + str(epoch) + '.png')
-    # with torch.no_grad():
-    #     sample = torch.randn(64, 20).to(device)
-    #     sample = model.decode(sample).cpu()
-    #     if dataset == 'MNIST':
-    #         save_image(sample.view(64, 1, 28, 28),
-    #             output_folder + '/sample_' + str(epoch) + '.png')
-    #     elif dataset == 'SVHN':
-    #         save_image(sample.view(64, 1, 32, 32), 
-    #             output_folder + '/sample_' + str(epoch) + '.png')
-        train_loss, bce_loss, kld_loss = train(epoch, model, train_loader, device, optimizer, log_interval)
-        test_loss, bce_test_loss, kld_test_loss = test(epoch, model, test_loader, device, batch_size, output_folder)
+        train_loss, recon_loss, kld_loss = train(epoch, model, train_loader, device, optimizer, log_interval, loss_type)
+        test_loss, recon_test_loss, kld_test_loss = test(epoch, model, test_loader, device, batch_size, output_folder, loss_type)
         total_train_loss.append(train_loss)
-        total_bce_loss.append(bce_loss)
+        total_recon_loss.append(recon_loss)
         total_kld_loss.append(kld_loss)
         total_test_loss.append(test_loss)
-        total_bce_test_loss.append(bce_test_loss)
+        total_recon_test_loss.append(recon_test_loss)
         total_kld_test_loss.append(kld_test_loss)
+        with torch.no_grad():
+            sample = torch.randn(64, 20).to(device)
+            sample = model.decode(sample).cpu()
+            if dataset == 'MNIST':
+                save_image(sample.view(64, 1, 28, 28),
+                    output_folder + '/sample_' + str(epoch) + '.png')
+            elif dataset == 'SVHN':
+                save_image(sample.view(64, 3, 32, 32), 
+                    output_folder + '/sample_' + str(epoch) + '.png')
 
     # plot loss figures 
     fix, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
 
     ax[0].plot(total_train_loss, color = 'blue', label='Training loss')
-    ax[0].plot(total_bce_loss, color='red', label='Training BCE loss')
+    ax[0].plot(total_recon_loss, color='red', label='Training Reconstruction loss')
     ax[0].plot(total_kld_loss, color='green', label='Training KLD loss')
     ax[0].set_xlabel('Epochs')
     ax[0].set_ylabel('Loss')
@@ -143,7 +146,7 @@ if __name__ == "__main__":
     ax[0].legend()
 
     ax[1].plot(total_test_loss, color = 'blue', label='Testing loss')
-    ax[1].plot(total_bce_test_loss, color='red', label='Testing BCE loss')
+    ax[1].plot(total_recon_test_loss, color='red', label='Testing Reconstruction loss')
     ax[1].plot(total_kld_test_loss, color='green', label='Testing KLD loss')
     ax[1].set_xlabel('Epochs')
     ax[1].set_ylabel('Loss')
@@ -158,7 +161,7 @@ if __name__ == "__main__":
     fix, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
 
     ax[0].plot(np.log(total_train_loss), color = 'blue', label='Training loss')
-    ax[0].plot(np.log(total_bce_loss), color='red', label='Training BCE loss')
+    ax[0].plot(np.log(total_recon_loss), color='red', label='Training Reconstruction loss')
     ax[0].plot(np.log(total_kld_loss), color='green', label='Training KLD loss')
     ax[0].set_xlabel('Epochs')    
     ax[0].set_ylabel('Loss')
@@ -166,7 +169,7 @@ if __name__ == "__main__":
     ax[0].legend()
 
     ax[1].plot(np.log(total_test_loss), color = 'blue', label='Testing loss')
-    ax[1].plot(np.log(total_bce_test_loss), color='red', label='Testing BCE loss')
+    ax[1].plot(np.log(total_recon_test_loss), color='red', label='Testing Reconstruction loss')
     ax[1].plot(np.log(total_kld_test_loss), color='green', label='Testing KLD loss')
     ax[1].set_xlabel('Epochs')
     ax[1].set_ylabel('Loss')

moemmvae.py
@@ -1,35 +1,70 @@
 # this file will have the model architecture for the MoE mmVAE model 
 # from __future__ import print_function
-import torch
-import torch.utils.data
-from torch import nn
-from torch.nn import functional as F
+import torch.distributions as dist
+from vae_mnist import*
+from vae_svhn import*
 
-class VAE(nn.Module):
-    def __init__(self):
-        super(VAE, self).__init__()
+class MMVAE(nn.Module):
+    def __init__(self, mnistvae_params, svhnvae_params, latent_dim, mnist_scale, svhn_scale):
+        super(MMVAE, self).__init__()
 
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        #self.mnistvae = VAE_MNIST(**mnistvae_params)
+        #self.svhnvae = VAE_SVHN(**svhnvae_params)
+        ## add parameter initializations in main class
+        self.mnistvae = VAE_MNIST(**mnistvae_params)
+        self.svhnvae = VAE_SVHN(**svhnvae_params)
+        self.vaes = nn.ModuleList([self.mnistvae, self.svhnvae])
+        self.M = len(self.vaes)
+        self.latent_dim = latent_dim
 
-    def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        # add scaling factors for calculating combined loss across the two modalities 
+        self.mnistvae.like_scale = mnist_scale
+        self.svhnvae.like_scale = svhn_scale
 
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
+    def forward(self, x, num_samples):
+        # forward pass through joint encoder distributions 
+        # qz_xs stores posteriors qz_x for each modality 
+        qz_xs = []
+        # zss stores the latent samples taken from each modality (num samples x batch size)
+        zss = []
+        # px_zs computes the cross-modal likelihood matrix. 
+        # ex. cross modal elements are computed by taking samples from encoder e and using the other modalities decoder
+        px_zs = [[None for num in range(self.M)] for num in range(self.M)]
 
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        for m, vae in enumerate(self.vaes):
+            # qz_x is the posterior distribution 
+            # px_z is the self-reconstruction likelihood (diagonal of px_zs)
+            # zs is the latent samples 
+            qz_x, px_z, zs = vae(x[m], num_samples)
+            qz_xs.append(qz_x)
+            zss.append(zs)
+            px_zs[m][m] = px_z
+        
+        # cross-modal decoding 
+        for e, zs in enumerate(zss):
+            for d, vae in enumerate(self.vaes):
+                if e != d:
+                    px_zs[e][d] = vae.px_z(vae.decode(zs))
+        
+        return qz_xs, zss, px_zs
+    
+    def pz(self):
+        device = next(self.mnistvae.parameters().device)
+        mu = torch.zeros(1, self.latent_dim, device=device)
+        sigma = torch.ones(1, self.latent_dim, device=device)
+        return dist.Normal(mu, sigma)
+    
+    # first initializing MoE ELBO loss and then will transition to calculating IWAE over K (num_samples)
+    def moe_elbo_loss(self, ):
 
-    def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+
+
+
+    
+        
+
+
+        
+    
+
 

train.py
@@ -5,23 +5,28 @@ import torch
 import torch.utils.data
 from torchvision.utils import save_image
 from loss_mse import*
+from loss_bce import*
 from utils import*
 
-def train(epoch, model, train_loader, device, optimizer, log_interval):
+def train(epoch, model, train_loader, device, optimizer, log_interval, loss_type):
     model.train()
     train_loss = 0
-    bce_loss = 0
+    recon_loss = 0
     kld_loss = 0
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
         # trying adding kl annealing 
-        beta = min(1.0, epoch / 10)
-        loss, bce, kld = loss_function(recon_batch, data, mu, logvar, beta)
+        #beta = min(1.0, epoch / 10)
+        beta = 1
+        if loss_type == 'bce':
+            loss, recon, kld = loss_function_mse(recon_batch, data, mu, logvar, beta)
+        elif loss_type == 'mse':
+            loss, recon, kld = loss_function_mse(recon_batch, data, mu, logvar, beta)
         loss.backward()
         train_loss += loss.item()
-        bce_loss += bce.item()
+        recon_loss += recon.item()
         kld_loss += kld.item()
         optimizer.step()
         if batch_idx % log_interval == 0:
@@ -31,45 +36,49 @@ def train(epoch, model, train_loader, device, optimizer, log_interval):
                 loss.item() / len(data)))
 
     train_loss /= len(train_loader.dataset)
-    bce_loss /= len(train_loader.dataset)
+    recon_loss /= len(train_loader.dataset)
     kld_loss /= len(train_loader.dataset)
     print('====> Epoch: {} Average loss: {:.4f}'.format(
           epoch, train_loss))
     
-    return train_loss, bce_loss, kld_loss
+    return train_loss, recon_loss, kld_loss
 
 
-def test(epoch, model, test_loader, device, batch_size, output_folder):
+def test(epoch, model, test_loader, device, batch_size, output_folder, loss_type):
     model.eval()
     test_loss = 0
-    bce_loss = 0
+    recon_loss = 0
     kld_loss = 0
     with torch.no_grad():
         for i, (data, _) in enumerate(test_loader):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
             # trying adding kl annealing 
-            beta = min(1.0, epoch / 10)
-            loss, bce, kld = loss_function(recon_batch, data, mu, logvar, beta)
+            #beta = min(1.0, epoch / 10)
+            beta = 1
+            if loss_type == 'bce':
+                loss, recon, kld = loss_function_mse(recon_batch, data, mu, logvar, beta)
+            elif loss_type == 'mse':
+                loss, recon, kld = loss_function_mse(recon_batch, data, mu, logvar, beta)
             test_loss += loss.item()
-            bce_loss += bce.item()
+            recon_loss += recon.item()
             kld_loss += kld.item()
             if i == 0:
                 n = min(data.size(0), 8)
                 if recon_batch.shape[-1] % 28 == 0:
                     comparison = torch.cat([data[:n],
                     recon_batch.view(batch_size, 1, 28, 28)[:n]])
-                    comparison = unnormalize(comparison, [0.1307], [0.3801])
+                    #comparison = unnormalize(comparison, [0.1307], [0.3801])
                 else:
                     comparison = torch.cat([data[:n],
                                        recon_batch.view(batch_size, 3, 32, 32)[:n]])
-                    comparison = unnormalize(comparison, (0.4376821, 0.4437697, 0.47280442), (0.19803012, 0.20101562, 0.19703614))
+                    #comparison = unnormalize(comparison, (0.4376821, 0.4437697, 0.47280442), (0.19803012, 0.20101562, 0.19703614))
                 save_image(comparison.cpu(),
                          output_folder + '/reconstruction_' + str(epoch) + '.png', nrow=n)
 
     test_loss /= len(test_loader.dataset)
-    bce_loss /= len(test_loader.dataset)
+    recon_loss /= len(test_loader.dataset)
     kld_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
-    return test_loss, bce_loss, kld_loss
+    return test_loss, recon_loss, kld_loss